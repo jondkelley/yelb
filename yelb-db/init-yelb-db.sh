a25171d1e9c300c2eafcda97a675243e8f5f4ed1
jondkelley <jonk@uberleet.org>
2021-10-29 17:18:34 -0400

    Revert "make changes to app to demo canary deployments"
    This reverts commit 6ee827160e0a0a39d9b7f63190fb9bd95bd6d121.

--- yelb-db/init-yelb-db.sh
+++ yelb-db/init-yelb-db.sh
@@ -9,9 +9,9 @@ psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
     	count       integer,
     	PRIMARY KEY (name)
 	);
-	INSERT INTO restaurants (name, count) VALUES ('chicfilet', 0);
-	INSERT INTO restaurants (name, count) VALUES ('whataburger', 0);
-	INSERT INTO restaurants (name, count) VALUES ('carls jr', 0);
-	INSERT INTO restaurants (name, count) VALUES ('culvers', 0);
+	INSERT INTO restaurants (name, count) VALUES ('outback', 0);
+	INSERT INTO restaurants (name, count) VALUES ('bucadibeppo', 0);
+	INSERT INTO restaurants (name, count) VALUES ('chipotle', 0);
+	INSERT INTO restaurants (name, count) VALUES ('ihop', 0);
 EOSQL
 
